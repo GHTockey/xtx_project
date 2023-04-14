f04bd520f9569f742fa46850fc1905298a840919
Tockey <tockey@yeah.net>
2023-04-14 10:28:01 +0800

    完成：商品详情图片放大镜效果

--- src/views/goods/components/GoodsImages.vue
+++ src/views/goods/components/GoodsImages.vue
@@ -2,10 +2,15 @@
 <template>
     <div class="goods-image">
         <!-- 放大 -->
-        <div class="large" v-show="show" :style="{ backgroundImage: `url(${mainPictures[current]})` }"></div>
-        <div class="middle"  ref="middleElement" >
+        <div class="large" v-show="show" :style="{
+            backgroundImage: `url(${mainPictures[current]})`,
+            backgroundPositionX: bgPosition.x + 'px',
+            backgroundPositionY: bgPosition.y + 'px',
+        }"></div>
+        <div class="middle" ref="middleElement">
             <img :src="mainPictures[current]" />
-            <div class="layer" v-show="show"></div>
+            <div class="layer" v-show="show" :style="{ top: layerPosition.top + 'px', left: layerPosition.left + 'px' }">
+            </div>
         </div>
         <ul class="small">
             <li :class="{ active: i === current }" v-for="item, i in mainPictures" :key="i" @mouseenter="current = i">
@@ -18,7 +23,7 @@
 <script lang="ts" setup>
 import { storeToRefs } from "pinia";
 import { useGoodsStore } from "@/stores/goodsStore";
-import { reactive, ref } from "vue";
+import { reactive, ref, watchEffect } from "vue";
 import { useMouseInElement } from "@vueuse/core";
 
 const goods_store = useGoodsStore();
@@ -37,6 +42,27 @@ const { isOutside, elementX, elementY } = useMouseInElement(middleElement);
 const layerPosition = reactive({ left: 0, top: 0 });
 // 用于记录大图位置
 const bgPosition = reactive({ x: 0, y: 0 });
+
+watchEffect(() => {
+    show.value = !isOutside.value
+    layerPosition.left = elementX.value - 100
+    layerPosition.top = elementY.value - 100
+    // 对镜片元素的水平方向位置进行限制
+    if (layerPosition.left < 0) {
+        layerPosition.left = 0;
+    } else if (layerPosition.left > 200) {
+        layerPosition.left = 200;
+    }
+    // 对镜片元素的垂直方向位置进行限制
+    if (layerPosition.top < 0) {
+        layerPosition.top = 0;
+    } else if (layerPosition.top > 200) {
+        layerPosition.top = 200;
+    }
+    // 记录大图位置
+    bgPosition.x = -layerPosition.left * 2;
+    bgPosition.y = -layerPosition.top * 2;
+})
 </script>
 
 <style scoped lang="less">
